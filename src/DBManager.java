ca1688bddfd4161c5ea37397513e54aa75c804f2
tianzhang92 <tz2237@columbia.edu>
2015-11-03 11:32:51 -0500

    update

--- src/DBManager.java
+++ src/DBManager.java
@@ -11,10 +11,10 @@ import com.google.gson.*;
 
 public class DBManager {
 	// Amazon RDS
-	private static final String DB_DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
-	private static String DB_CONNECTION_URL;
-	private static String DB_USER_NAME;
-	private static String DB_PASSWORD;
+	private static final String DB_DRIVER_CLASS_NAME = "";
+	private static String DB_CONNECTION_URL = "";
+	private static String DB_USER_NAME = "";
+	private static String DB_PASSWORD = "";
 	
 	public Connection connection = null;
 	
@@ -52,29 +52,43 @@ public class DBManager {
 		}
 	}
 	
-	public ArrayList<String> getData(int interval){
-		TimeZone zone = TimeZone.getTimeZone("UTC");
-		Calendar curTime = Calendar.getInstance(zone);
-		//Calendar curTime = Calendar.getInstance();
-		curTime.add(Calendar.SECOND, -interval);
-		java.sql.Timestamp cur = new java.sql.Timestamp(curTime.getTimeInMillis());
+	//public ArrayList<String> getData(int interval){
+	public ArrayList<String> getData(){
+		int count=0;
+		//TimeZone zone = TimeZone.getTimeZone("UTC");
+		//Calendar curTime = Calendar.getInstance(zone);
+		////Calendar curTime = Calendar.getInstance();
+		//curTime.add(Calendar.SECOND, -interval);
+		//java.sql.Timestamp cur = new java.sql.Timestamp(curTime.getTimeInMillis());
 		ArrayList<String> data = new ArrayList<String>();
 		PreparedStatement ps = null;
-		ResultSet rs = null;		
-		String query = "select Latitude, Longtitude, keyword from tweet where tweetTimeStamp > ? and keyword IS NOT NULL";
+		ResultSet rs = null;	
+		
+		//String query = "select Latitude, Longtitude, keyword, score from tweet where tweetTimeStamp > ? and keyword IS NOT NULL";
+		String query = "select Latitude, Longtitude, keyword, score from tweet where ID>50";
+
+		System.out.println(query);
 		try {
 			ps = connection.prepareStatement(query);
-			ps.setTimestamp(1, cur);
-			System.out.println(cur);
+			//ps.setTimestamp(1, cur);
+			//System.out.println(cur);
 			rs = ps.executeQuery();
 			while(rs.next()){
 				Double lati = rs.getDouble(1);
 				Double longti = rs.getDouble(2);
 				String keyword = rs.getString(3);
+				Double score = rs.getDouble(4);
 				Gson gson = new Gson();
-				TweetInfo geoData = new TweetInfo(lati, longti, keyword);
+				TweetInfo geoData = new TweetInfo(lati, longti, keyword,score);
 				String entry = gson.toJson(geoData);
 				data.add(entry);
+				TweetInfo geoDataSendAll = new TweetInfo(lati, longti,"all",score);
+				entry = gson.toJson(geoDataSendAll);
+				data.add(entry);
+				count++;
+				if(count==50){
+					break;
+				}
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -88,9 +102,11 @@ class TweetInfo {
 	double lat;
 	double lon;
 	String kw;
-	public TweetInfo(double lati, double longti, String keyword){
+	double senti;
+	public TweetInfo(double lati, double longti, String keyword,double senti){
 		this.lat = lati;
 		this.lon = longti;
 		this.kw = keyword;
+		this.senti = senti;
 	}	
 }
